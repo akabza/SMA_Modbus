1c2982661e898a8be2c8be28d90618162d6498d6
Alexander Kabza <99689771+akabza@users.noreply.github.com>
2022-03-18 21:58:47 +0100

    Update SMA_Modbus.py

--- SMA_Modbus.py
+++ SMA_Modbus.py
@@ -1,31 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# created by Alexander Kabza, 2022-02-21
-# 2022-02-21    first version
-# 2022-02-22    minor mods
+# read_sma_modbus.py, based on https://github.com/JanusHL/pvcontrol/tree/master/SMA
+# created by Alexander Kabza, 2020-02-26
+# 2022-03-18    changed from Python2 to Python3
 
 import os
+import datetime
 import time
 import sys
 from pyModbusTCP.client import ModbusClient
+
 import ctypes
 
 class x2u16Struct(ctypes.Structure):
     _fields_ = [("h", ctypes.c_uint16),
                 ("l", ctypes.c_uint16)]
+
+
 class convert(ctypes.Union):
     _fields_ = [("float", ctypes.c_float),
                 ("u16", x2u16Struct),
                 ("sint32", ctypes.c_int32),
                 ("uint32", ctypes.c_uint32)]
 
-TCP_IP = '192.168.X.Y'
-UNIT_ID = 3
-PORT = 502
-
 def getvalue(reg, len):
-    #data = mbus.read_data(reg, len)
+    # data = mbus.read_data(reg, len)
     data = mb_device.read_holding_registers(int(reg), len)
     Translate = convert()
     Translate.u16.h = data[1]
@@ -33,8 +33,15 @@ def getvalue(reg, len):
     value = Translate.uint32
     return value
 
+
 if __name__ == "__main__":
-    now = (time.strftime("%Y-%m-%d ") + time.strftime("%H:%M:%S"))
+
+    TCP_IP = '192.168.144.54'
+    UNIT_ID = 3
+    PORT = 502
+
+    now = datetime.datetime.now()
+    nowstr = time.strftime("%Y-%m-%d %H:%M:%S")
 
     try:
         mb_device = ModbusClient(host=TCP_IP, port=PORT, timeout=10, debug=True, unit_id=UNIT_ID)
@@ -47,18 +54,10 @@ if __name__ == "__main__":
     SerNo = getvalue(30005, 2)
     PowerGrid = getvalue(30865, 2)
     DeviceClass = getvalue(30051, 2)
-    CondMode = getvalue(30201, 2)
+    Condition = getvalue(30201, 2)
     OpHrs = getvalue(30541, 2) / 3600.0
-    FeedInHrs = getvalue(30543, 2)
-    if (FeedInHrs >= int("FFFFFFFF", 16)):  # >= 4,294,967.29, 0x100000000 = 4,294,967,295
-        FeedInHrs = PrevFeedInHrs
-    else:
-        FeedInHrs = FeedInHrs / 3600.0
-    TotalYield = getvalue(30529, 4)
-    if (TotalYield >= int("FFFFFFFF", 16)):  # >= 4,294,967.29, 0x100000000 = 4,294,967,295
-        TotalYield = PrevTotalYield
-    else:
-        TotalYield = TotalYield / 1000.0
+    FeedInHrs = getvalue(30543, 2) / 3600.0
+    TotalYield = getvalue(30529, 4) / 1000.0
     TotalGrid = getvalue(30581, 2) / 1000.0
     TotalFeedIn = getvalue(30583, 2) / 1000.0
     PowerGrid = getvalue(30865, 2)
@@ -66,13 +65,19 @@ if __name__ == "__main__":
     PowerPV = getvalue(30775, 2)
     if (PowerPV == int("80000000", 16)):
         PowerPV = 0.0
-    U1 = getvalue(31253, 2) / 100.0
-    U2 = getvalue(31255, 2) / 100.0
-    U3 = getvalue(31257, 2) / 100.0
+    UL1 = getvalue(30783, 2) / 100.0
+    UL2 = getvalue(30785, 2) / 100.0
+    UL3 = getvalue(30787, 2) / 100.0
+    UPPCL1 = getvalue(31253, 2) / 100.0
+    UPPCL2 = getvalue(31255, 2) / 100.0
+    UPPCL3 = getvalue(31257, 2) / 100.0
+    UL1L2 = getvalue(30789, 2) / 100.0
+    UL2L3 = getvalue(30791, 2) / 100.0
+    UL3L1 = getvalue(30793, 2) / 100.0
 
     print("SerNo = ", SerNo)
     print("PowerGrid = {:d} W".format(PowerGrid))
-    print("CondMode = ", CondMode)
+    print("Condition = ", Condition)
     print("OpHrs = {0:0.2f} hrs".format(OpHrs))
     print("FeedInHrs = {0:0.2f} hrs".format(FeedInHrs))
     print("TotalYield = {0:0.2f} kWh".format(TotalYield))
@@ -81,9 +86,14 @@ if __name__ == "__main__":
     print("PowerGrid = {0:0.0f} W".format(PowerGrid))
     print("PowerFeed = {0:0.0f} W".format(PowerFeed))
     print("PowerPV = {0:0.0f} W".format(PowerPV))
-    print("U1 = {0:0.2f} V".format(U1))
-    print("U2 = {0:0.2f} V".format(U2))
-    print("U3 = {0:0.2f} V\n".format(U3))
+    print("UL1 = {0:0.2f} V".format(UL1))
+    print("UL2 = {0:0.2f} V".format(UL2))
+    print("UL3 = {0:0.2f} V".format(UL3))
+    print("UPPCL1 = {0:0.2f} V".format(UPPCL1))
+    print("UPPCL2 = {0:0.2f} V".format(UPPCL2))
+    print("UPPCL3 = {0:0.2f} V".format(UPPCL3))
+    print("UL1L2 = {0:0.2f} V".format(UL1L2))
+    print("UL2L3 = {0:0.2f} V".format(UL2L3))
+    print("UL3L1 = {0:0.2f} V\n".format(UL3L1))
 
     mb_device.close()
-	
